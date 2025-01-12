332092cbe1055a08be5074c6d04f968ae11ac63e
Gustavo Kovalski Saporiti <gustavo.saporiti@me.com.br>
2025-01-12 14:37:48 -0300

    Md5TokenProvider unit tests

--- Application/Providers/Md5TokenProvider.cs
+++ Application/Providers/Md5TokenProvider.cs
@@ -7,6 +7,12 @@ public class Md5TokenProvider : ITokenGenerator
 {
     public Guid GenerateToken(string cardNumber, string cvv)
     {
+        if (string.IsNullOrEmpty(cardNumber))
+            throw new ArgumentException("Card number is required");
+
+        if (string.IsNullOrEmpty(cvv))
+            throw new ArgumentException("CVV is required");
+
         using var md5 = MD5.Create();
         var input = $"{cardNumber}{cvv}";
         var hash = md5.ComputeHash(Encoding.UTF8.GetBytes(input));
